49b9b4a67020f6786afabfc2355827d4d572c33a
chicoumbetov <shyngysmaaan@gmail.com>
2023-07-07 16:58:36 +0200

    add py files

---
+++ check-200.py
@@ -0,0 +1,97 @@
+﻿import sys
+import requests
+
+headers_discord = {
+    "Accept": "application/json"
+}
+
+suffixes = {
+    "prod": "",
+    "rc": "-rc"
+}
+
+
+def get_urls(env):
+    suffix = suffixes.get(env)
+    return {
+        "web": f"https://www{suffix}.site.com/fr/objets-bim",
+        "web-api": f"https://www{suffix}.site.com/api",
+        "bo": f"https://bo{suffix}.site.com/",
+        "bo-api": f"https://bo{suffix}.site.com/api",
+        "analytics": f"https://analytics{suffix}.site.com/",
+        "analytics-api": f"https://analytics{suffix}.site.com/api",
+        "auth": f"https://auth{suffix}.site.com/",
+        "auth-api": f"https://auth{suffix}.site.com/api/.well-known/openid-configuration",
+    }
+
+
+def get_testing_urls(env):
+    return {
+        "web": f"https://{env}.site.com/fr/objets-bim",
+        "web-api": f"https://{env}.site.com/api",
+        "bo": f"https://{env}bo.site.com/",
+        "bo-api": f"https://{env}bo.site.com/api",
+        "analytics": f"https://{env}analytics.site.com/",
+        "analytics-api": f"https://{env}analytics.site.com/api",
+        "auth": f"https://{env}auth.site.com/",
+        "auth-api": f"https://{env}auth.site.com/api/.well-known/openid-configuration",
+    }
+
+
+def check_all(env):
+    if env in suffixes.keys():
+        urls = get_urls(env)
+    elif env in ["preprod", "test"]:
+        urls = get_testing_urls(env)
+    else:
+        error_discord("Misusage - accepted envs are : prod, rc, preprod, test")
+        return
+
+    results = []
+
+    for site, url in urls.items():
+        try:
+            res = requests.get(url, allow_redirects=False, timeout=60)
+            status = res.status_code
+        except:
+            pass
+        finally:
+            if status is None:
+                value = f" :x: UNKNOWN"
+            elif status == 200 or status == 302:
+                value = f":white_check_mark: {status}"
+            else:
+                value = f":x: **{status}**"
+                
+            results.append({
+                "name": site,
+                "url": url,
+                "value": value,
+                "inline": True
+            })
+            
+
+    send_discord({
+        "embeds": [
+            {
+                "title": f"{env.upper()} - status checks results",
+                "color": 15258703,
+                "fields": results
+            }
+        ]
+    })
+
+
+def error_discord(message):
+    send_discord(f"An error occured. In chicoumbetov/cicd > workflow deploy-checks > check-200.py. Details : {message}")
+
+
+def send_discord(message):
+    requests.post("https://discord.com/api/webhooks/ ...."
+                  "...",
+                  headers=headers_discord, json=message)
+
+
+if len(sys.argv) == 2:
+    check_all(sys.argv[1])
+
---
+++ json_checks.py
@@ -0,0 +1,48 @@
+﻿import glob
+import json
+import re
+
+
+def is_valid(json_filename):
+    with open(json_filename) as json_file:
+        json_str = ""
+        try:
+            json_str = json_file.read()
+            json_str = json_str.encode().decode('utf-8-sig')
+
+            json_str = re.sub("^ *\t*//.*$", "", json_str, flags=re.MULTILINE)
+            json_str = re.sub(", *\t*//.*", ",", json_str, flags=re.MULTILINE)
+            json_str = re.sub("\\\\\"", "", json_str, flags=re.MULTILINE)
+            json_str = re.sub(", *\n *\t*}", "\n}", json_str, flags=re.MULTILINE)
+            json_str = re.sub(", *\n *\t*]", "\n]", json_str, flags=re.MULTILINE)
+
+            json.loads(json_str)
+            return True, "OK"
+        except ValueError as err:
+            return False, "" + json_filename + " is NOT valid, error: " + str(err)
+        except UnicodeDecodeError as err:
+            return False, "" + json_filename + " is NOT valid, error: " + str(err)
+
+
+ok_json_filenames = []
+ko_json_results = []
+
+for filename in glob.glob('./**/*.json', recursive=True):
+    if "appsettings" in filename:
+        res, msg = is_valid(filename)
+        if res:
+            ok_json_filenames.append(filename)
+        else:
+            ko_json_results.append({"filename": filename, "msg": msg})
+
+if len(ok_json_filenames) > 0:
+    print("Those files successfully passed the JSON check :")
+    print("\n".join(ok_json_filenames))
+    print("\n\n")
+
+if len(ko_json_results) > 0:
+    print("Those files FAILED the JSON check (see error message) :")
+    for result in ko_json_results:
+        print(result["filename"], result["msg"])
+        print("\n\n")
+    raise ValueError()
